073f01923047aaf563fea314973513feda82597b
flexkat <catherinemaritz1@gmail.com>
2021-09-06 22:37:55 +1200

    Install storybook and add Card story

---
+++ client/src/Card.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Card from "./Card";
+
+export default {
+  title: "Components/Card",
+  component: Card,
+  argTypes: {
+    to: { control: false },
+    handleClick: { action: 'clicked' },
+  },
+} as ComponentMeta<typeof Card>;
+
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
+
+export const example = Template.bind({});
+example.args = {
+  title: "Title",
+  author: "Author"
+};
